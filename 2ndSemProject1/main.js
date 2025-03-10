eaa9feffbc9bf60b7c98004df4bebb739779a92b
KristalLibre <82042670+dbutiting@users.noreply.github.com>
2025-03-10 22:28:44 +0800

    Add files via upload

---
+++ 2ndSemProject1/main.js
@@ -0,0 +1,52 @@
+const Greeting = document.getElementById("Greeting"); 
+Greeting.style.backgroundColor = "Beige";
+Greeting.style.padding = "100px";
+Greeting.style.borderRadius = "50%";
+Greeting.style.color = "#d3d366";
+Greeting.style.fontSize = "75px";
+Greeting.style.margin = "50px 100px 25px 100px";
+
+
+document.body.style.textAlign = "Center";
+document.body.style.fontSize = "30px";
+
+
+const Farewell = document.getElementById("Farewell");
+Farewell.style.backgroundColor = "#ffe9ec";
+Farewell.style.padding = "100px";
+Farewell.style.borderRadius = "50%";
+Farewell.style.color = "#ff8396";
+Farewell.style.fontSize = "100px";
+Greeting.style.margin = "50px 100px 25px 100px";
+
+
+const talkBack = document.getElementById("Response");
+talkBack.style.backgroundColor = "Gray";
+talkBack.style.padding = "25px";
+talkBack.style.margin = "100px";
+talkBack.style.borderRadius = "12px";
+talkBack.style.color = "White";
+talkBack.style.fontSize = "50px";
+talkBack.style.cursor = "pointer";
+
+
+let Reply = document.getElementById("Response1");
+Reply.style.width = "400px";
+Reply.style.height = "100px";
+Reply.style.textAlign = "Center";
+Reply.style.fontSize = "50px";
+Reply.style.border = "5px solid black";
+Reply.style.borderRadius = "20px";
+alert(Reply);
+ 
+
+function Clicky(){
+const Insult = document.getElementById("Insult");
+Insult.innerHTML = "``Sucker.``";
+Insult.style.backgroundColor = "#2d2d2d";
+Insult.style.padding = "100px";
+Insult.style.borderRadius = "50%";
+Insult.style.color = "#ff4c00";
+Insult.style.fontSize = "150px";
+Insult.style.margin = "50px 100px 25px 100px";
+}
